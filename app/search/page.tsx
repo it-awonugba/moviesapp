bdd55bc1fb77966505a4a3c7861e9f2a63b2925b
Isaac <it.awonugba@gmail.com>
2025-06-06 23:46:13 +0100

    bug fix

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
-import { useEffect, useState, Suspense } from "react";
+import { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
 import { TMovieList } from "@/types";
 
